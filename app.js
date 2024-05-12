4cd73fa4252ea7b69f316236ffd2a11638997c05
Sambit Kumar Mondal <sambitmondal02@gmail.com>
2024-05-12 22:40:31 +0530

    changed files

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 
-const http = require('node:http');
-
+import http from "node:http"
 
 const PORT = process.env.PORT || 5000;
-
+const path = "./index.html"
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end("./index.html");
 });
 
 server.listen(PORT,() => {
