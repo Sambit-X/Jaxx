be30a171d8a10572ef10e1596053da0af1229484
Sambit Kumar Mondal <sambitmondal02@gmail.com>
2024-05-23 13:55:13 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <>
+      <Navbar></Navbar>
       <p>Supratims one and only T shirt website</p>
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="./src/assets/logo.jpg" alt="logo here" width={"100px"} height="100px" />
+      <p>This is a Navbar</p>
+      <ul>
+        <li>Home</li>
+        <li>Shop</li>
+        <li>Customer Support</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
